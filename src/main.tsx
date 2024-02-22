1c77cc7bbcc561e29ad56aecdfd7bacc480fd78f
Almaz <almazzz-2@yandex.ru>
2024-02-22 22:31:42 +0300

    fix 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { Helmet } from 'react-helmet';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Helmet>
+      <title>TEST</title>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
